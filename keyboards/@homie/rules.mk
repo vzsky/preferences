1be768eb060c6d57730c3c5b6dc846c90bbb3409
vzsky <my999n@gmail.com>
2024-02-19 21:50:30 +0900

    rework homie

---
+++ keyboards/@homie/rules.mk
@@ -0,0 +1,6 @@
+# This file intentionally left blank
+COMBO_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+OLED_ENABLE = yes
+LTO_ENABLE = yes
+# TAP_DANCE_ENABLE = yes
--- keyboards/caelum/keymaps/default/keymap.c
+++ keyboards/caelum/keymaps/default/keymap.c
@@ -11,14 +11,15 @@ enum layers {
 };
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE, 
+  QWERTY = SAFE_RANGE,
   COLEMAK
 };
 
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
-#define NAV MO(_NAV)
+#define NAV    MO(_NAV)
+#define NAV_BK LT(_NAV, KC_BSPC)
 
 #define CLEFT  C(KC_LEFT)
 #define CRIGHT C(KC_RIGHT)
@@ -34,20 +35,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,           KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_CAPS,
         KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,           KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         NAV    , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,           KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
-        KC_LSFT,          KC_LCTL, KC_LGUI, LOWER  , KC_SPC ,           KC_BSPC, RAISE  , KC_MPLY, KC_MNXT,          KC_ENT
+        KC_LSFT,          KC_LCTL, KC_LGUI, LOWER  , KC_SPC ,           NAV_BK , RAISE  , KC_MPLY, KC_MNXT,          KC_ENT
     ),
     [_COLEMAK] = LAYOUT(
         KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,           KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_CAPS,
         KC_ESC , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,           KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,
         NAV    , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,           KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
-        KC_LSFT,          KC_LCTL, KC_LGUI, LOWER  , KC_SPC ,           KC_BSPC, RAISE  , KC_MPLY, KC_MNXT,          KC_ENT
+        KC_LSFT,          KC_LCTL, KC_LGUI, LOWER  , KC_SPC ,           NAV_BK , RAISE  , KC_MPLY, KC_MNXT,          KC_ENT
     ),
     [_NAV] = LAYOUT(
-        XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , XXXXXXX,           COPY,    XXXXXXX, XXXXXXX, XXXXXXX, PATSE  , XXXXXXX, 
+        XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , XXXXXXX,           COPY,    XXXXXXX, XXXXXXX, XXXXXXX, PATSE  , XXXXXXX,
         XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , XXXXXXX,           KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
         _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX,           LWORD,   KC_PGDN, KC_PGUP, RWORD ,  XXXXXXX, XXXXXXX,
         _______,          _______, _______, _______, _______,           _______, _______, _______, _______,          _______
-                                       
+
     ),
     [_LOWER] = LAYOUT(
         XXXXXXX, KC_UNDS, KC_AMPR, KC_ASTR, KC_GRV,  KC_TILD,           XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______,           _______, _______, _______, _______,          _______
     ),
     [_RAISE] = LAYOUT(
-        XXXXXXX, KC_UNDS, KC_7   , KC_8   , KC_9   , XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       
+        XXXXXXX, KC_UNDS, KC_7   , KC_8   , KC_9   , XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, KC_4   , KC_5   , KC_6   , XXXXXXX,           XXXXXXX, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, XXXXXXX,
         _______, KC_0   , KC_1   , KC_2   , KC_3   , XXXXXXX,           XXXXXXX, KC_CIRC, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX,
         CLEFT  ,          _______, _______, _______, _______,           _______, _______, _______, _______,          CRIGHT
