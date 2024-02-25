9d4839a00714d0f864e27f8ce04176bc96a7f77a
vzsky <my999n@gmail.com>
2024-02-25 23:20:37 +0900

    renew planck

--- keyboards/@compactyl/compactyl.c
+++
@@ -1,1 +0,0 @@
-#include "compactyl.h"
--- keyboards/@homie/rules.mk
+++
@@ -1,6 +0,0 @@
-# This file intentionally left blank
-COMBO_ENABLE = yes
-REPEAT_KEY_ENABLE = yes
-OLED_ENABLE = yes
-LTO_ENABLE = yes
-# TAP_DANCE_ENABLE = yes
