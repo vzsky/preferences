b7473df1239062fafe8df29c76487a42551e7fa3
Touch Sungkawichai <my999n@gmail.com>
2025-11-13 07:48:34 +0000

    more testing

--- keyboards/caelum/keymaps/mako/keymap.c
+++ keyboards/caelum/keymaps/mako/keymap.c
@@ -19,7 +19,7 @@ enum layers {
 #define SCR_LFT LCA(KC_LEFT)
 
 #define TMUX_LEADER LCTL(KC_B)
-#define TMUX_CMD(x) tap_code16(TMUX_LEADER); tap_code16(x); tap_code16(KC_ESC);
+#define TMUX_CMD(x) tap_code16(TMUX_LEADER); tap_code16(x);
 
 enum custom_keycodes {
     // TMUX
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_F12 , KC_F7  , KC_F8  , KC_F9  , XXXXXXX,           XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, KC_F11 , KC_F4  , KC_F5  , KC_F6  , XXXXXXX,           KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
         XXXXXXX, KC_F10 , KC_F1  , KC_F2  , KC_F3  , XXXXXXX,           XXXXXXX, MS_BTN1, MS_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,
-        SCR_LFT,          XXXXXXX, XXXXXXX, _______, _______,           _______, _______, XXXXXXX, XXXXXXX,          SCR_RGH
+        _______,          XXXXXXX, XXXXXXX, _______, _______,           _______, _______, XXXXXXX, XXXXXXX,          _______
 
     ),
     [_LOWER] = LAYOUT(
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
         XXXXXXX, KC_UNDS, KC_7   , KC_8   , KC_9   , XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, KC_4   , KC_5   , KC_6   , XXXXXXX,           XXXXXXX, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, XXXXXXX,
-        _______, KC_0   , KC_1   , KC_2   , KC_3   , XXXXXXX,           XXXXXXX, KC_CIRC, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_0   , KC_1   , KC_2   , KC_3   , XXXXXXX,           XXXXXXX, KC_CIRC, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX,
         SCR_LFT,          XXXXXXX, XXXXXXX, _______, _______,           _______, _______, XXXXXXX, XXXXXXX,          SCR_RGH
     ),
     [_TMUX] = LAYOUT(
