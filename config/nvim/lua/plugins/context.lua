dfceb13ebd872f4db71023a5233dc36104690950
Touch Sungkawichai <my999n@gmail.com>
2026-02-19 21:53:16 +0000

    edit context

--- config/nvim/lua/plugins/context.lua
+++ config/nvim/lua/plugins/context.lua
@@ -1,7 +1,11 @@
+vim.g.context_max_per_indent = 3
+vim.g.context_skip_regex = '^\\s*\\($\\|#\\|//\\|/\\*\\|\\*($\\|\\s*$\\)'
+vim.g.context_extend_regex =
+  '^\\s*\\([\\[\\]{}()]\\|end\\|else\\|\\(case\\|default\\|done\\|elif\\|fi\\)\\>\\|public:\\|private:\\|protected:\\)'
+
 return {
   'wellle/context.vim',
   config = function ()
-    vim.g.context_presenter = "nvim_float"
-    vim.g.context_max_per_indent = 3
+    -- set here doesn't work
   end
 }
